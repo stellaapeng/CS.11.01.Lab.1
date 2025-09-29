b850f4131e213f030a0ae4b28cd81c51e2537b00
Stella <stellap2028@student.cis.edu.hk>
2025-09-29 14:10:36 +0800

    grftvbt

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
+	
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");	
+        int age = scanner.nextInt();   
+        int dateable_age = (age/2)+7;
+        System.out.println(age + "-year olds should date somebody who is at least " + dateable_age + " years old."); 
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your age:");	
+        int age = scanner.nextInt();   
+        int pages_req = 100-age;
+        System.out.println(age + "-year olds should read at least " + pages_req + " before giving up on a book."); 
     }
 }
